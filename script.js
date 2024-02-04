da6779700547e8c7d4b97b7e365439f1d5484c6b
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-02-04 15:35:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ document.querySelector('.card-holder-input').oninput = () =>{
     document.querySelector('.card-holder-name').innerText = document.querySelector('.card-holder-input').value;
 }
 
+document.querySelector('.month-input').oninput = () =>{
+    document.querySelector('.exp-month').innerText = document.querySelector('.month-input').value;
+}
+
+document.querySelector('.year-input').oninput = () =>{
+    document.querySelector('.exp-year').innerText = document.querySelector('.year-input').value;
+}
+
+document.querySelector('.cvv-input').onmouseenter = () =>{
+    document.querySelector('.front').style.transform = 'perspective(1000px) rotateY(-180deg)';
+    document.querySelector('.back').style.transform = 'perspective(1000px) rotateY(0deg)';
+}
